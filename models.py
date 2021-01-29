a8ae7e15fcb4db5798a0e3b27d588f99ee636716
tomvonheill <tomvonheill@gmail.com>
2021-01-29 10:44:50 -0600

    changed database URI to heroku database

--- models.py
+++ models.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 import json
 import os
 
-database_path = os.environ['DATABASE_URL']
+database_path = os.environ['HEROKU_POSTGRESQL_YELLOW_URL']
 
 db = SQLAlchemy()
 
