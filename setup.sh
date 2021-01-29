f3883c5144a1071854424df34aebbd0e97e6c3bb
tomvonheill <tomvonheill@gmail.com>
2021-01-29 10:55:55 -0600

    added migration files

--- setup.sh
+++ setup.sh
@@ -1,2 +1,3 @@
 export DATABASE_URL=postgresql://postgres:postgres@localhost/heroku_test_db
 export EXCITED=true
+export HEROKU_POSTGRESQL_YELLOW_URL=postgres://thsgajovevhrhp:b0196945dc61db66d05e28d4725b919b2281deab916d07bf8c5931cc84b68b04@ec2-52-204-113-104.compute-1.amazonaws.com:5432/deo2dv7na0ie2l
