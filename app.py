5eb9be329c42949b30aa8d611f02abbc56b33a37
tomvonheill <tomvonheill@gmail.com>
2021-01-29 11:50:27 -0600

    added new database version

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask
 from models import setup_db
 from flask_cors import CORS
+from flask_migrate import Migrate
 
 def create_app(test_config=None):
 
--- models.py
+++ models.py
@@ -29,6 +29,7 @@ class Person(db.Model):
   id = Column(Integer, primary_key=True)
   name = Column(String)
   catchphrase = Column(String)
+  aditionaldata=Column(String)
 
   def __init__(self, name, catchphrase=""):
     self.name = name
